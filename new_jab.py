b0191e852df9d4f48c68771e9936d78cfcba4680
edoardousai <158064033+edoardousai@users.noreply.github.com>
2024-02-15 20:09:01 +0000

    Add files via upload

--- new_jab.py
+++ new_jab.py
@@ -1,6 +1,7 @@
 from llama_index import SimpleDirectoryReader
 from llama_index.node_parser import SimpleNodeParser
-
+import os
+os.environ['OPENAI_API_KEY']='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'
 # load in some sample data
 reader = SimpleDirectoryReader(input_files=["Rawdata (2).json"])
 documents = reader.load_data()
@@ -13,8 +14,8 @@ from llama_index.llama_pack import download_llama_pack
 
 HybridFusionRetrieverPack = download_llama_pack(
     "HybridFusionRetrieverPack",
-    "./hybrid_fusion_pack"
-)
+    "./hybrid_fusion_pack")
 hybrid_fusion_pack = HybridFusionRetrieverPack(
-    nodes, chunk_size=256, vector_similarity_top_k=2, bm25_similarity_top_k=2
-)
+    nodes, chunk_size=256, vector_similarity_top_k=2, bm25_similarity_top_k=2)
+response = hybrid_fusion_pack.run("give me feynmann plots")
+print(str(response))
